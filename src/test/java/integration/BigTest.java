94a351a9c616e800d2a5c7567bb1016e214c1916
ShawnWu <shawn.wu@linkage-retail.com>
2023-10-14 16:02:41 +0800

    Initial commit

---
+++ src/test/java/integration/BigTest.java
@@ -0,0 +1,14 @@
+package integration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BigTest {
+
+
+
+    @Test
+    public void test_false() {
+        Assertions.assertTrue(false);
+    }
+}
